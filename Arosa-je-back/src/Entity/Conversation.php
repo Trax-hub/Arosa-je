8642870187230ac5be0cb27b420130db39937aa2
Trax-hub <78899389+Trax-hub@users.noreply.github.com>
2023-04-25 09:43:45 +0200

    ajout api platform

--- Arosa-je-back/src/Entity/Conversation.php
+++ Arosa-je-back/src/Entity/Conversation.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ConversationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ConversationRepository::class)]
+#[ApiResource]
 class Conversation
 {
     #[ORM\Id]
