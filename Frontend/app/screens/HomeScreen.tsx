66760e4562199ef66fe55832a5a51e98a50aa05b
LORY Arthur <arthur.lory@epsi.fr>
2023-06-01 16:41:48 +0200

    card

--- Frontend/app/screens/HomeScreen.tsx
+++ Frontend/app/screens/HomeScreen.tsx
@@ -1,50 +1,69 @@
-import React, { FC } from "react"
-import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { AppStackScreenProps } from "app/navigators"
-import { Button, Card, Screen, Text } from "app/components"
-import { useNavigation } from "@react-navigation/native"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "app/models"
+import React, { FC } from "react";
+import { observer } from "mobx-react-lite";
+import { View, ViewStyle, Dimensions, StyleSheet } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { AppStackScreenProps } from "app/navigators";
+import { Button, Card, Screen, Text } from "app/components";
+import { useNavigation } from "@react-navigation/native";
 
 interface HomeScreenProps extends NativeStackScreenProps<AppStackScreenProps<"Home">> {}
 
-export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+// Get the window width
+const windowWidth = Dimensions.get('window').width;
 
-  // Pull in navigation via hook
-  const navigation = useNavigation()
+// Define styles
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: windowWidth > 768 ? 'row' : 'column',
+    justifyContent: 'center', // Center items on the main axis
+    alignItems: 'center', // Center items on the cross axis
+  },
+});
+
+export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <Screen style={$root} preset="scroll">
-      <Card
-        verticalAlignment="center"
-        LeftComponent={<Text>Left</Text>}
-        RightComponent={<Text>Right</Text>}
-        heading="Card Heading"
-        content="Card Content"
-        FooterComponent={<Button preset="reversed" text="FooterComponent"/>}
-      />
-      <Card
-        verticalAlignment="center"
-        LeftComponent={<Text>Left</Text>}
-        RightComponent={<Text>Right</Text>}
-        heading="Card Heading"
-        content="Card Content"
-        FooterComponent={<Button preset="reversed" text="FooterComponent"/>}
-      />
-      <Card
-        verticalAlignment="center"
-        LeftComponent={<Text>Left</Text>}
-        RightComponent={<Text>Right</Text>}
-        heading="Card Heading"
-        content="Card Content"
-        FooterComponent={<Button preset="reversed" text="FooterComponent"/>}
-      />
+      <View style={styles.cardContainer}>
+        <Card
+          verticalAlignment="center"
+          heading="Conseils de pro"
+          content="Trouvez les réponses à vos questions"
+          FooterComponent={<Button 
+            preset="reversed" 
+            text="Découvrir"
+            style={{ backgroundColor: '#2F5E3D' }} 
+            textStyle={{ color: '#FFFFFF' }} 
+          />}
+        />
+        <Card
+          verticalAlignment="center"
+          heading="Carte interactive"
+          content="Pour trouver les plantes à garder près de chez vous"
+          FooterComponent={<Button 
+            preset="reversed" 
+            text="Découvrir"
+            style={{ backgroundColor: '#2F5E3D' }} 
+            textStyle={{ color: '#FFFFFF' }} 
+          />}
+        />
+        <Card
+          verticalAlignment="center"
+
+          heading="Scanner une plante"
+          content="Trouvez les réponses à vos questions"
+          FooterComponent={<Button 
+            preset="reversed" 
+            text="Découvrir"
+            style={{ backgroundColor: '#2F5E3D' }} 
+            textStyle={{ color: '#FFFFFF' }} 
+          />}
+        />
+      </View>
     </Screen>
   )
-})
+});
+
 
 const $root: ViewStyle = {
   flex: 1,
