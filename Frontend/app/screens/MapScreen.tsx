a2b654f917642f392c6cc290528b8df16fc24594
LORY Arthur <arthur.lory@epsi.fr>
2023-06-06 00:19:36 +0200

    test

--- Frontend/app/screens/MapScreen.tsx
+++ Frontend/app/screens/MapScreen.tsx
@@ -1,28 +1,46 @@
-import React, { FC } from "react"
-import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { AppStackScreenProps } from "app/navigators"
-import { Screen, Text } from "app/components"
-import MapView from 'react-native-maps';
+import React, { FC, useEffect, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { ViewStyle } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { AppStackScreenProps } from "app/navigators";
+import { Screen, Text } from "app/components";
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "app/models"
+import axios from "axios";
 
 interface MapScreenProps extends NativeStackScreenProps<AppStackScreenProps<"Map">> {}
 
 export const MapScreen: FC<MapScreenProps> = observer(function MapScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const [plants, setPlants] = useState([]);
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  useEffect(() => {
+    axios.get("http://172.20.10.5:8000/api/posts")
+      .then((response) => {
+        setPlants(response.data["hydra:member"]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        {plants.map((plant, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: plant.latitude, longitude: plant.longitude }}
+          >
+            <Callout>
+              <Text>{plant.title}</Text>
+              <Text>{plant.description}</Text>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
-})
+});
 
 const styles = StyleSheet.create({
   container: {
